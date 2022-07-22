e1308406b26a4ea8accfd26dd8c538ce9d7f05d9
anguangchao <anguangchao@yintatech.com>
2022-07-22 16:43:22 +0800

    Initial commit

---
+++ hello_word.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/7/22 16:19
+# @Author  : ...
+# @File    : hello_word.py
+# @chnages    : 1.
+
+print('Hello Word!')
+
+if __name__ == '__main__':
+    pass
